b19fcd74ed2284df709a9264b8e4ca12f601e495
AnaMaria2604 <ana.26.62.maria.04@gmail.com>
2026-05-22 08:43:26 +0300

    Refactor confidence interval output and add output file

--- Labs/lab12/lab12a.py
+++ Labs/lab12/lab12a.py
@@ -18,9 +18,3 @@ for confidence in [0.90, 0.95, 0.99]:
     start = m - h
     end = m + h
     print("confidence:", confidence, "start: ", start, " end: ", end)
-
-# n:  12
-# mean:  12.666666666666666
-# confidence: 0.9 start:  9.59297796408904  end:  15.740355369244293
-# confidence: 0.95 start:  8.8996415486546  end:  16.433691784678732
-# confidence: 0.99 start:  7.351023759181143  end:  17.98230957415219
